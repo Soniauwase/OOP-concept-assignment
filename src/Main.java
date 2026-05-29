5cdc8ba5620ea2cc5e0b79b4f6d1437b4efb2429
Soniauwase <soniauwase65@gmail.com>
2026-05-29 12:12:16 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,11 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-          Employee emp1 = new Employee(12,"Cynthia", "Manager", 124000);
-        Employee emp2 = new Employee(1,"Fanny", "Developer", 390000);
-        Employee emp3 = new Employee(12,"Delucie", "Manager", 309000);
 
-        List<Employee> sonia = new ArrayList<>();
-//        employees.add(emp1);
-//        employees.add(emp2);
-//        employees.add(emp3);
-//        System.out.println(employees);
 
         Scanner sc = new Scanner(System.in);
 
-        for (int loop = 1 ; loop <= 3 ; loop ++){
+        for (int loop = 1; loop <= 3; loop++) {
             System.out.println("*******employee " + loop + " details *****");
             System.out.print("ENTER YOUR ID: ");
             int id = sc.nextInt();
@@ -28,18 +20,18 @@ public class Main {
             String status = sc.next();
             System.out.print("ENTER YOUR SALARY: ");
             double salary = sc.nextDouble();
+            Employee employee=new Employee(id,name,status,salary);
+            System.out.println(employee);
 
-            Employee chapati = new Employee(id,name,status,salary);
-            if(chapati.getEmployee_sstatus().equalsIgnoreCase("manager")){
-                sonia.add(chapati);
-            }
+
+//
+//
+//
+        }
+//
+//
 
 
         }
 
-        System.out.println(sonia);
-
-
     }
-
-}
